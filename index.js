a359bbb163a69825d9f5178d157b1aebf3e42220
kunal <kunalwaldia8@gmail.com>
2023-11-30 13:15:53 +0530

    index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ let connection = mysql.createConnection({
     password: "#Kunal8@"
 });
 
-app.use(express.urlencoded({ extended: true }));
 app.set("view engines", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
 
 app.listen("8080", () => {
     console.log("listening port 8080");
